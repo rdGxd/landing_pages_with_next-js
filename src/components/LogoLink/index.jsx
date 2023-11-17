7776659fa1ecce87ccb5bb1fcfd2f5057f1d9da8
Rodrigo <rdg.hora1@gmail.com>
2023-11-17 09:03:38 -0300

    Add next/link

--- src/components/LogoLink/index.jsx
+++ src/components/LogoLink/index.jsx
@@ -1,8 +1,24 @@
+import Link from "next/link";
 import P from "prop-types";
 import { Heading } from "../Heading/index.jsx";
 import * as Styled from "./styles.js";
 
 export const LogoLink = ({ text, srcImg = "", link }) => {
+  const nextLink = link.match(/^\//) ? true : false;
+
+  if (nextLink) {
+    return (
+      <Heading size="small" uppercase>
+        <Link href={link} passHref>
+          <Styled.Container>
+            {!!srcImg && <img src={srcImg} alt={text} />}
+            {!srcImg && text}
+          </Styled.Container>
+        </Link>
+      </Heading>
+    );
+  }
+
   return (
     <Heading size="small" uppercase>
       <Styled.Container href={link}>{srcImg ? <img src={srcImg} alt={text} /> : text}</Styled.Container>
